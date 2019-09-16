0f855f69df45801020c5bd5f744ac7fe41280239
juliana Cristina Gonçalves <juliana@ipe.digital>
2019-09-16 17:58:34 -0300

    Category chalenge - Work in progress

--- src/main/java/com/iftm/curso/resources/CategoryResources.java
+++ src/main/java/com/iftm/curso/resources/CategoryResources.java
@@ -4,19 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.iftm.curso.entities.Category;
 import com.iftm.curso.services.CategoryService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value =  "/categories")
 public class CategoryResources {
 	
 	@Autowired
+
 	private CategoryService service;
 	
 	@GetMapping
@@ -29,5 +28,28 @@ public class CategoryResources {
 	public ResponseEntity<Category> findById(@PathVariable Long id){
 		Category obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
-	}		
+	}
+
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{id}")
+				.buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id,@RequestBody Category obj ){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
+
 } 
