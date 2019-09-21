92de93b64421fd20b38a344ba3d15c8ef0678892
juliana Cristina Gonçalves <juliana@ipe.digital>
2019-09-21 15:48:22 -0300

    Custom Validation Response

--- src/main/java/com/iftm/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/iftm/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.iftm.curso.services.exceptions.DatabaseException;
 import com.iftm.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -31,5 +33,19 @@ public class ResourceExceptionHandler {
 
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e , HttpServletRequest request){
+        String error = "Validation error";
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+
+        for (FieldError x : e.getBindingResult().getFieldErrors() ){
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+
 
 }
