e5f7b0f413b85f797b7b06ebb1169fc503c24975
JulianaCristina <julianacristina780@gmail.com>
2019-11-16 19:07:19 -0300

    Pre-authorizing requests

--- src/main/java/com/iftm/curso/resources/CategoryResources.java
+++ src/main/java/com/iftm/curso/resources/CategoryResources.java
@@ -7,6 +7,7 @@ import com.iftm.curso.dto.CategoryDTO;
 import com.iftm.curso.dto.CategoryInsertDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.iftm.curso.entities.Category;
@@ -31,7 +32,7 @@ public class CategoryResources {
 		CategoryDTO obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryInsertDTO dto){
 		CategoryDTO newDTO = service.insert(dto);
@@ -39,13 +40,13 @@ public class CategoryResources {
 
 		return ResponseEntity.created(uri).body(newDTO);
 	}
-
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> update(@PathVariable Long id,@RequestBody CategoryDTO dto){
 		dto = service.update(id, dto);
